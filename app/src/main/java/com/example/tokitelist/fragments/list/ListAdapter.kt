0c18d8c6760ba443372e1923effa9bce8b9145ec
Nikolaus Koopmann <nikolauskoopmann@gmail.com>
2020-10-26 13:32:30 +0100

    will try something hereafter

--- app/src/main/java/com/example/tokitelist/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/tokitelist/fragments/list/ListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tokitelist.R
 import com.example.tokitelist.data.models.KiteItem
@@ -24,6 +25,9 @@ class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.itemView.txt_name.text = dataList[position].name
+        holder.itemView.row_background.setOnClickListener{
+            holder.itemView.findNavController().navigate(R.id.action_listFragment_to_editFragment)
+        }
 
         when (dataList[position].season) {
             Season.summer -> holder.itemView.season_indicator.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.hot))
