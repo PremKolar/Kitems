080c2b570da8330f3e974552e59b64f6ed921ce2
Nikolaus Koopmann <nikolauskoopmann@gmail.com>
2020-11-19 11:12:08 +0100

    wip

--- app/src/main/java/com/nk/tokitelist/data/ToKiteDB.kt
+++ app/src/main/java/com/nk/tokitelist/data/ToKiteDB.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 
 
-@Database(entities = [KiteItem::class, KiteSession::class, Spot::class], version = 1, exportSchema = false)
+@Database(entities = [KiteItem::class, KiteSession::class, Spot::class], version = 2, exportSchema = false)
 @TypeConverters(Converter::class)
 abstract class ToKiteDB: RoomDatabase() {
     abstract fun toKiteDao():ToKiteDao
@@ -25,9 +25,6 @@ abstract class ToKiteDB: RoomDatabase() {
         private var INSTANCE: ToKiteDB? = null
 
         fun getDataBase(context: Context): ToKiteDB{
-
-
-
             val tempInstance = INSTANCE
             if(tempInstance!=null){
                 return tempInstance
@@ -42,23 +39,22 @@ abstract class ToKiteDB: RoomDatabase() {
                 val rdc: Callback = object : Callback() {
                     override fun onCreate(db: SupportSQLiteDatabase) {
                         var idx = 1
-//                        idx = addKitem("Kite small",Season.always,idx)
-//                        idx = addKitem("Kite large",Season.always,idx)
-//                        idx = addKitem("Harness",Season.always,idx)
-//                        idx = addKitem("Bar",Season.always,idx)
-//                        idx = addKitem("Leash",Season.always,idx)
-//                        idx = addKitem("Pump",Season.always,idx)
-//                        idx = addKitem("Kiteboard",Season.always,idx)
-//                        idx = addKitem("Water",Season.always,idx)
-//                        idx = addKitem("Food",Season.always,idx)
-//                        idx = addKitem("Cash",Season.always,idx)
-//                        idx = addKitem("Towel",Season.always,idx)
-//                        idx = addKitem("Sunscreen",Season.summer,idx)
-//                        idx = addKitem("Surf Shorts",Season.summer,idx)
-//                        idx = addKitem("Hood",Season.winter,idx)
-//                        idx = addKitem("Booties",Season.winter,idx)
-                        idx = addKitem("Gloves",Season.winter,idx)
-
+                        idx = addKitem("Kite small",Season.always,idx)
+                        idx = addKitem("Kite large",Season.always,idx)
+                        idx = addKitem("Harness",Season.always,idx)
+                        idx = addKitem("Bar",Season.always,idx)
+                        idx = addKitem("Leash",Season.always,idx)
+                        idx = addKitem("Pump",Season.always,idx)
+                        idx = addKitem("Kiteboard",Season.always,idx)
+                        idx = addKitem("Water",Season.always,idx)
+                        idx = addKitem("Food",Season.always,idx)
+                        idx = addKitem("Cash",Season.always,idx)
+                        idx = addKitem("Towel",Season.always,idx)
+                        idx = addKitem("Sunscreen",Season.summer,idx)
+                        idx = addKitem("Surf Shorts",Season.summer,idx)
+                        idx = addKitem("Hood",Season.winter,idx)
+                        idx = addKitem("Booties",Season.winter,idx)
+                        addKitem("Gloves",Season.winter,idx)
                     }
 
 //                override fun onOpen(db: SupportSQLiteDatabase) {
